e99f08e071e90e930eae71df4026bcbb31bc5d43
Katy Ashworth <katyashworth27@gmail.com>
2024-12-24 22:13:26 +0000

    Add user friendly message to handle for GitHub API throttling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -63,7 +63,11 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
-            } else {
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Rest')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until${resetTime.toLocaleDateString()}</h4>`)
+
+            } else{
                 console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
